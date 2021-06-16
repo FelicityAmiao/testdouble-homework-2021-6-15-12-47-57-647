2a44b7bf2c620d4669dd10f17f6f3770a4127ba1
CHENFE2 <felicity.chen@cargosmart.com>
2021-06-16 23:14:09 +0800

    [Felicity]test: TransactionRepository can add deposit

--- src/test/java/com/tw/banking/TransactionRepositoryTest.java
+++ src/test/java/com/tw/banking/TransactionRepositoryTest.java
@@ -2,6 +2,7 @@ package com.tw.banking;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -17,5 +18,18 @@ class TransactionRepositoryTest {
     assertThrows(UnsupportedOperationException.class, () -> {
       transactions.add(new Transaction("23/03/2018", 1));
     });
+  }
+
+  @Test
+  void should_add_one_transaction_with_currentDate_and_amount_when_call_addDeposit_given_amount() {
+    Clock stubClock = mock(Clock.class);
+    TransactionRepository transactionRepository = new TransactionRepository(stubClock);
+    when(stubClock.todayAsString()).thenReturn("16/06/2021");
+
+    transactionRepository.addDeposit(1);
+
+    List<Transaction> result = transactionRepository.allTransactions();
+    assertEquals("16/06/2021", result.get(0).date());
+    assertEquals(1, result.get(0).amount());
   }
 }
