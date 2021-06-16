e6f67265dd6b32fcc3e0b2ba28b231d474e267f4
CHENFE2 <felicity.chen@cargosmart.com>
2021-06-16 22:50:45 +0800

    [Felicity]test: Transaction can return -1 when compare fail

--- src/test/java/com/tw/banking/TransactionTest.java
+++ src/test/java/com/tw/banking/TransactionTest.java
@@ -15,4 +15,14 @@ class TransactionTest {
 
     assertEquals(1, result);
   }
+
+  @Test
+  void should_return_minus_1_when_call_compareTo_given_transaction_big() {
+    Transaction given = new Transaction("23/02/2019", 1);
+    Transaction transaction = new Transaction("23/02/2018", 1);
+
+    int result = transaction.compareTo(given);
+
+    assertEquals(-1, result);
+  }
 }
